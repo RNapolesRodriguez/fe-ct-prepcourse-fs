57e1b77fe72824b84f7ef9c79a8386ef4d1496d0
RNapolesRodriguez <138038385+RNapolesRodriguez@users.noreply.github.com>
2023-07-09 18:29:22 -0300

    feat: Solve modulo3 PrepCourse

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -4,37 +4,43 @@
 function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
-   // Tu código:
-}
+   // Tu código: 
+   return x===y
+} 
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+  return str1.length ===str2.length
 }
 
-function menosQueNoventa(num) {
+const menosQueNoventa = (num) => num < 90 
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
-}
+    
+
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   return num > 50
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   return num % 2 === 0
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   return num % 2 !== 0
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
